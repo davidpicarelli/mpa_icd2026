6c2d5d1a0dbffba95e65f9a3bd6987479400cc53
davidpicarelli <davidpicarelli@gmail.com>
2026-04-09 22:39:15 -0300

    aula 5: funcoes, purrr e quarto

---
+++ lista-exercicios/04-lista/04-lista-resolucao.R
@@ -0,0 +1,40 @@
+# Arquivo: 04-lista-resolucao.R
+# Autor(a): David Picarelli Gonçalves
+# Data: 09/04/2026
+# Objetivo: Resolução da lista de exercícios 4
+
+# Configuracoes globais  ------------------------------------
+
+# define opções globais para exibição de números
+options(digits = 5, scipen = 999)
+
+# carrega os pacotes necessários
+library(here)      # para usar caminhos relativos
+library(tidyverse) # inclui readr, dplyr, tidyr, ggplot2 etc.
+
+
+# Exercício 1 ------------------------------------------------
+
+
+
+# Exercício 2 ------------------------------------------------
+
+
+
+# Exercício 3 ------------------------------------------------
+
+
+
+# Exercício 4 ------------------------------------------------
+
+
+
+# Exercício 5 ------------------------------------------------
+
+
+
+# Exercício 6 ------------------------------------------------
+# (resolver em arquivo .qmd separado)
+
+
+# Exercício 7 (Desafio) --------------------------------------
